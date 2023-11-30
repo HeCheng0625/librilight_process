c59cbfb2e34b0c9082f0bcf38cc264b159636934
AlanSwift <shenkai200861@gmail.com>
2023-11-30 11:42:51 +0000

    commit

--- auto_asr.sh
+++ auto_asr.sh
@@ -1,4 +1,4 @@
-python /home/v-detaixin/librilight_process/auto_asr.py \
+/opt/conda/envs/codec/bin/python /home/v-detaixin/librilight_process/auto_asr.py \
     --in_dir="/home/v-detaixin/librilight/small_cut" \
     --out_dir="/home/v-detaixin/librilight/small_processed" \
     --device_id="1" \
@@ -6,4 +6,4 @@ python /home/v-detaixin/librilight_process/auto_asr.py \
     --min_silence_len=500 \
     --keep_silence=500 \
     --spk_num_start=0 \
-    --spk_num_end=10 \
+    --spk_num_end=100 \
