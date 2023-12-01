3da664f529ce0a6993c7f3698ceec8b5046f6019
AlanSwift <shenkai200861@gmail.com>
2023-12-01 03:10:46 +0000

    commit

--- auto_asr_medium.sh
+++ auto_asr_medium.sh
@@ -1,12 +1,12 @@
 /opt/conda/envs/codec/bin/python /home/v-detaixin/librilight_process/auto_asr.py \
     --in_dir="/home/v-detaixin/librilight/medium_cut" \
     --out_dir="/home/v-detaixin/librilight/medium_processed" \
-    --device_id="3" \
+    --device_id="7" \
     --export_chunk_len=7500 \
     --min_silence_len=500 \
     --keep_silence=500 \
-    --spk_num_start=0 \
-    --spk_num_end=100 \
+    --spk_num_start=600 \
+    --spk_num_end=700 \
 
 
 # GPU 3: 0 - 100
