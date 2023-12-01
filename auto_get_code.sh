73f875405a9488c4263ee040ea961e7955e14b1d
yuanchengwang <119010319@link.cuhk.edu.cn>
2023-12-01 06:53:48 +0000

    commit

---
+++ auto_get_code.sh
@@ -0,0 +1,6 @@
+/home/v-zeqianju/.conda/envs/codec/bin/python /home/v-zeqianju/librilight_process/auto_get_codec.py \
+    --in_dir="/home/v-zeqianju/librilight/small_processed" \
+    --out_dir="/home/v-zeqianju/librilight/small_acoustic_encodec" \
+    --device_id="0" \
+    --spk_num_start=0 \
+    --spk_num_end=100 \
--- auto_get_codec.py
+++ auto_get_codec.py
@@ -1,14 +1,25 @@
 from encodec import EncodecModel
 from encodec.utils import convert_audio
-
 import torchaudio
 import torch
+import os
+import numpy as np
+import argparse
+from tqdm import tqdm
 
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description="Build the statistics on LibriBig")
+    parser.add_argument('--in_dir', type=str)
+    parser.add_argument('--out_dir', type=str)
+    parser.add_argument('--device_id', type=int)
+    parser.add_argument('--spk_num_start', type=int, default=0)
+    parser.add_argument('--spk_num_end', type=int, default=100)
+    args = parser.parse_args()
 
-    device = ...
+    device = "cuda:{}".format(str(args.device_id))
+
     model = EncodecModel.encodec_model_24khz()
     model.set_target_bandwidth(6.0)
     model.to(device)
@@ -26,4 +37,33 @@ if __name__ == "__main__":
             
             return codes
 
-    
+    input_dir = args.in_dir
+    out_dir = args.out_dir
+
+    for spk_id in tqdm(sorted(os.listdir(input_dir))[args.spk_num_start: args.spk_num_end]):
+        print("speaker:", spk_id)
+        spk_path = os.path.join(input_dir, spk_id)
+        if len(os.listdir(spk_path)) == 0:
+            continue
+        for chapter_id in sorted(os.listdir(spk_path)):
+            print("chapter:", chapter_id)
+            chapter_path = os.path.join(spk_path, chapter_id)
+            if len(os.listdir(chapter_path)) == 0:
+                continue
+            for wav_name in sorted(os.listdir(chapter_path)):
+                if not wav_name.endswith(".wav"):
+                    continue
+                print(wav_name)
+                wav_path = os.path.join(chapter_path, wav_name)
+                out_folder = os.path.join(out_dir, spk_id, chapter_id)
+
+                if not os.path.isdir(out_folder):
+                    os.makedirs(out_folder, exist_ok=True)    
+                
+                out_path = os.path.join(out_dir, spk_id, chapter_id, wav_name.replace(".wav", ".npy"))
+
+                try:
+                    code = extract_encodec_token(wav_path)
+                except:
+                    continue
+                np.save(out_path, code)
